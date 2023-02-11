fd256d158fb691fbee8db5d3b0062b9fa0f7adf0
Eric M Shen <eric.mm.shen@gmai.com>
2023-02-11 14:41:32 -0500

    grpc files

--- client.py
+++ client.py
@@ -1,7 +1,13 @@
 import socket
+import sys
 
-HOST = "127.0.0.1"  # The server's hostname or IP address
-PORT = 65432  # The port used by the server
+PORT = 22067  # The port used by the server
+
+if len(sys.argv) != 2:
+    print(f"Usage: {sys.argv[0]} <host>")
+    sys.exit(1)
+    
+HOST = sys.argv[1]
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
